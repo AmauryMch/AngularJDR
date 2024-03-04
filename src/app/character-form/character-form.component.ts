314471b025140636f4797832e9f7936abc266e07
Amaury <amaurymechin37@gmail.com>
2024-03-04 16:00:20 +0100

    formulaire du personnage

--- src/app/character-form/character-form.component.ts
+++ src/app/character-form/character-form.component.ts
@@ -10,20 +10,4 @@ import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
 })
 export class CharacterFormComponent {
 
-  characterForm = new FormGroup({
-    characterName: new FormControl(''),
-    classAndLevel: new FormControl(''),
-    race: new FormControl(''),
-    background: new FormControl(''),
-    alignment: new FormControl(''),
-    playerName: new FormControl(''),
-    experiencePoints: new FormControl(''),
-  })
-
-  selectedRace = "";
-
-  onSelected(value: string): void {
-    this.selectedRace = value;
-  }
-
 }
