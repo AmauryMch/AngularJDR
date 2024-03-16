69de6f3635280aaf8a93dc6867689e7da613fa7f
Amaury <amaurymechin37@gmail.com>
2024-03-16 19:18:29 +0100

    export pdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
 import { CharacterFormComponent } from "./character-form/character-form.component";
 import { AttributeFormComponent } from "./attribute-form/attribute-form.component";
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [CharacterFormComponent, AttributeFormComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CharacterFormComponent, AttributeFormComponent]
 })
 export class AppComponent {
   title = 'AngularJDR';
+
+  save() {
+    const element = document.getElementById('contentToConvert');
+    const today = new Date();
+    const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+
+    const pdfWidth = 210;
+
+    const button = document.querySelector('button');
+
+    if (button) {
+      button.style.display = 'none';
+    }
+
+    if (element) {
+      html2canvas(element).then(canvas => {
+        const imgData = canvas.toDataURL('image/png');
+
+        const img = new Image();
+        img.src = imgData;
+
+        const imgWidth = canvas.width;
+        const imgHeight = canvas.height;
+
+        const ratio = pdfWidth / imgWidth;
+        const pdfHeight = imgHeight * ratio;
+
+        const pdf = new jsPDF('p', 'mm', 'a4');
+
+        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+
+        pdf.save(`fiche_personnage_${date}.pdf`);
+
+        if (button) {
+          button.style.display = 'block'; // Réaffiche le bouton après avoir sauvegardé le PDF
+        }
+      });
+    } else {
+      console.error('Element with id "contentToConvert" not found.');
+    }
+  }
+
+
+
 }
